ab8e353f25c159dd9c111c28c2a8f783a9c19219
HappyNoose <78141897+MrZahourek@users.noreply.github.com>
2026-02-27 19:36:05 +0100

    Update musicPlayer.js

--- public/assets/js/musicPlayer.js
+++ public/assets/js/musicPlayer.js
@@ -1,6 +1,16 @@
 class Song {
-    constructor(scr) {
+    constructor(scr, songName) {
+        this.audio = new Audio();
         this.scr = scr;
-        this.audio.scr = scr;
+        this.isLoaded = false;
+
+        this.songName = songName;
+
+        this.setup();
+    }
+
+    setup() {
+        this.audio.src = this.scr;
+        this.audio.oncanplaythrough = () => {this.isLoaded = true}
     }
 }

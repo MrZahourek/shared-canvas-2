b21f0a9b10ed3bc4840deb32695b698d525b3c4c
HappyNoose <78141897+MrZahourek@users.noreply.github.com>
2026-02-27 06:40:23 +0100

    working on init

--- app/models/Canvas.php
+++ app/models/Canvas.php
@@ -2,24 +2,31 @@
 
 class Canvas {
 
-    public static function findByName($name, $db) {
-        return $db->runQuery(
-            "SELECT * FROM canvas_configs WHERE canvas_name = ?",
-            [$name]
-        )->fetch();
+    public ?string $canvas_name;
+    public ?string $config;
+    public ?string $snap;
+
+    public function __constructor($canvas_name) {
+        $this->canvas_name = $canvas_name;
     }
 
-    public static function getSnapshot($name, $db) {
-        return $db->runQuery(
-            "SELECT snapshot FROM canvas_snapshots ORDER BY snapshotID DESC WHERE canvas_name = ? LIMIT 1",
-            [$name]
-        )->fetch();
+    public function getConfig($db) {
+        $this->config = $db->runQuery("select config from canvas_configs where canvas_name = ?", [$this->canvas_name]);
+        return $this->config;
+    }
+    public function getSnapshot($db) {
+        $this->snap =  $db->runQuery("SELECT last_edit_id, snapshot FROM canvas_snapshots WHERE canvas_name = ? order by snapshotID desc limit 1", [$this->canvas_name])->fetch();
+        return $this->snap;
     }
 
-    public static function getEdits($lastID, $name, $db) {
-        return $db->runQuery(
-            "SELECT * FROM edit_history ORDER BY editID ASC WHERE editID => ? AND canvas_name = ?",
-            [$lastID, $name]
-        )->fetch();
+    public function getEdits($db, $lastID) {
+        $edits = [];
+        $sql = "select x, y, color from edit_history where canvas_name = ? and editID < ? order by editID asc limit 1";
     }
+
+    public function newEdit($db, $color, $x, $y) {}
+
+    public function newSnapshot($db) {}
+
+
 }
